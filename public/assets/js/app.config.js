bd4188f21c88399f3e65e8afad39328e77345336
F. Elombat <f.elombat@gmail.com>
2020-04-10 20:46:48 +0100

    AngularJS rendering template big

---
+++ public/assets/js/app.config.js
@@ -0,0 +1,25 @@
+(function(){
+    angular.
+        module('spaApp').
+        config(['$routeProvider',
+            function config($routeProvider) {
+                $routeProvider.
+                    when('/people', {
+                        templateUrl: 'phonelist/list' 
+                    }).
+                    when('/people/:personId', {
+                        template: '<person-detail></person-detail>'
+                    }).
+                    when('/books', {
+                        templateUrl: 'library/books'
+                    }).
+                    when('/news', {
+                        templateUrl: 'news'
+                    }).
+                    otherwise('/people');
+            }
+        ])
+        .constant('appSettings', { serverPath: "http://localhost/ngfuel-spa/public/api/v1/" });
+
+    
+}());
